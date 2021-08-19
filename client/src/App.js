ee326820e4c1a92ee9952e368f31023b5a3756c3
TravisSLee <travisslee223@gmail.com>
2021-08-19 17:23:44 -0400

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import Login from './Login';
 
+
+const code = new URLSearchParams(window.location.search).get(code)
 function App() {
   return (
     < Login/>
