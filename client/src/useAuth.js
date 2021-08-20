42100f1d586aa84e212a4db7ce8b07ded3cf7fd3
TravisSLee <travisslee223@gmail.com>
2021-08-20 15:01:14 -0400

    update client

---
+++ client/src/useAuth.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+
+export default function useAuth(code) {
+   const [ accessToken, setAccessToken] = useState()
+   const [ refreshToken, setRefreshToken] = useState()
+   const [expiresIn, setExpiresIn] = useState()
+
+   useEffect(() => {
+    
+   }, [code])
+}
+
