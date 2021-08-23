494960c920948ca56debca9c38d5d12c885bdad2
TravisSLee <travisslee223@gmail.com>
2021-08-23 16:05:59 -0400

    update client features

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,6 +2,7 @@ import { useState,useEffect } from 'react'
 import { Container, Form } from 'react-bootstrap'
 import useAuth from './useAuth'
 import SpotifyWebApi from 'spotify-web-api-node'
+import TrackSearchResult from './TrackSearchResult'
 
 const spotifyApi = new SpotifyWebApi({
     clientId: "1c4cb0c2c13744d68e94385ebff0a485",
@@ -21,26 +22,52 @@ export default function Dahboard({ code }) {
 
     useEffect(() => {
         if (!search) return setSearchResults([])
-        if (!accessToken) return 
-
+        if (!accessToken) return
+    
+        let cancel = false
         spotifyApi.searchTracks(search).then(res => {
+          if (cancel) return
+          setSearchResults(
             res.body.tracks.items.map(track => {
-                return {
-                    artist: track.artists[0].name,
-                    title: track.name, 
-                    uri: track.uri,
-                    albumUrl: track.albumUrl.images,
-
-                }
+              const smallestAlbumImage = track.album.images.reduce(
+                (smallest, image) => {
+                  if (image.height < smallest.height) return image
+                  return smallest
+                },
+                track.album.images[0]
+              )
+    
+              return {
+                artist: track.artists[0].name,
+                title: track.name,
+                uri: track.uri,
+                albumUrl: smallestAlbumImage.url,
+              }
             })
+          )
         })
-    }, [search, accessToken])
+    
+        return () => (cancel = true)
+      }, [search, accessToken])
+
     return (
         <div>
             <Container className="d-flex dlex-column py-2" style={{height: "100vh"}}>
                 { code }
                 <Form.Control type="search" placeholder="Search Songs/ Artists" value={search} onChange={e => e.target.value} />
-                <div className="flex-grow-1 my-2" style={{ overFlow: "auto" }}></div>
+                <div className="flex-grow-1 my-2" style={{ overFlow: "auto" }}>
+                {searchResults.map(track => (
+                    <TrackSearchResult
+                        track={track}
+                        key={track.uri}
+                        chooseTrack={chooseTrack}
+                    />
+                    ))}
+                </div>
+
+                <div>
+                    <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+                </div>
             </Container>
         </div>
     )
